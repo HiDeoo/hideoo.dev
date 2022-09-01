8a8876ff6c3f9520731fedcfd90dc0af6bffaaea
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2022-09-01 19:27:23 +0200

    build: setup astro

---
+++ .prettierrc.js
@@ -0,0 +1,19 @@
+const baseConfig = require('@hideoo/prettier-config')
+
+/**
+ * @type {import('prettier').Config}
+ */
+const customPrettierConfig = {
+  ...baseConfig,
+  plugins: [require.resolve('prettier-plugin-astro')],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+}
+
+module.exports = customPrettierConfig
