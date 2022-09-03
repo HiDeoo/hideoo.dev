0bce71a4af707f1a5ba37eb9daf56430fd871a53
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2022-09-03 14:54:40 +0200

    refactor: switch to open props instead of tailwind

--- astro.config.ts
+++
@@ -1,8 +0,0 @@
-import tailwind from '@astrojs/tailwind'
-import { defineConfig } from 'astro/config'
-
-const astroConfig = defineConfig({
-  integrations: [tailwind()],
-})
-
-export default astroConfig
--- tailwind.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-const tailwdindConfig = {
-  content: ['src/**/*.{astro,html,ts,tsx}'],
-  darkMode: 'class',
-}
-
-export default tailwdindConfig
