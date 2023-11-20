7195becf50531f021206aac5192de5707390a9b5
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2023-11-20 16:01:15 +0100

    feat: notes pagination

--- src/libs/note.ts
+++ src/libs/note.ts
@@ -6,47 +6,75 @@ const dateFormat = new Intl.DateTimeFormat('en-US', { dateStyle: 'medium' })
 const datetimeFormat = new Intl.DateTimeFormat('en-US', { dateStyle: 'short' })
 
 export async function getNotes(count?: number): Promise<Note[]> {
-  const notes = await getCollection('notes')
-  const sortedNotes = notes
-    .sort((a, b) => b.data.publishDate.getTime() - a.data.publishDate.getTime())
-    .map((aNote) => {
-      // Not using remark so we can use the reading time in the note list.
-      let readingTimeMinutes = Math.floor(readingTime(aNote.body).minutes)
+  const rawNotes = await getCollection('notes')
+  const sortedNotes = rawNotes.sort((a, b) => b.data.publishDate.getTime() - a.data.publishDate.getTime())
+  const notes = sortedNotes.map((aNote, index) => {
+    // Not using remark so we can use the reading time in the note list.
+    let readingTimeMinutes = Math.floor(readingTime(aNote.body).minutes)
 
-      if (readingTimeMinutes < 1) {
-        readingTimeMinutes = 1
+    if (readingTimeMinutes < 1) {
+      readingTimeMinutes = 1
+    }
+
+    const note: Note = {
+      ...aNote,
+      href: getNoteHref(aNote),
+      publishDate: dateFormat.format(aNote.data.publishDate),
+      publishDatetime: datetimeFormat.format(aNote.data.publishDate),
+      readingDatetime: serialize({ minutes: readingTimeMinutes }),
+      readingTime: `${readingTimeMinutes}min`,
+    }
+
+    if (aNote.data.updateDate) {
+      note.updateDate = dateFormat.format(aNote.data.updateDate)
+      note.updateDatetime = datetimeFormat.format(aNote.data.updateDate)
+    }
+
+    const prevNote = sortedNotes[index + 1]
+
+    if (prevNote) {
+      note.next = {
+        href: getNoteHref(prevNote),
+        title: prevNote.data.title,
       }
+    }
 
-      const note: Note = {
-        ...aNote,
-        href: `/notes/${aNote.slug}`,
-        publishDate: dateFormat.format(aNote.data.publishDate),
-        publishDatetime: datetimeFormat.format(aNote.data.publishDate),
-        readingDatetime: serialize({ minutes: readingTimeMinutes }),
-        readingTime: `${readingTimeMinutes}min`,
+    const nextNote = sortedNotes[index - 1]
+
+    if (nextNote) {
+      note.prev = {
+        href: getNoteHref(nextNote),
+        title: nextNote.data.title,
       }
+    }
 
-      if (aNote.data.updateDate) {
-        note.updateDate = dateFormat.format(aNote.data.updateDate)
-        note.updateDatetime = datetimeFormat.format(aNote.data.updateDate)
-      }
-
-      return note
-    })
+    return note
+  })
 
   if (count) {
-    return sortedNotes.slice(0, count)
+    return notes.slice(0, count)
   }
 
-  return sortedNotes
+  return notes
+}
+
+function getNoteHref(note: CollectionEntry<'notes'>) {
+  return `/notes/${note.slug}`
 }
 
 export type Note = CollectionEntry<'notes'> & {
   href: string
+  next?: PrevNextNote
+  prev?: PrevNextNote
   publishDate: string
   publishDatetime: string
   readingDatetime: string
   readingTime: string
   updateDate?: string
   updateDatetime?: string
+}
+
+interface PrevNextNote {
+  href: string
+  title: string
 }
