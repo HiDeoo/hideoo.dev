5e95d22153eec2f9ef5a924682cd328b397f1b1b
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2023-11-20 12:01:49 +0100

    feat: note reading time

--- src/libs/note.ts
+++ src/libs/note.ts
@@ -1,4 +1,6 @@
 import { getCollection, type CollectionEntry } from 'astro:content'
+import readingTime from 'reading-time'
+import { serialize } from 'tinyduration'
 
 const dateFormat = new Intl.DateTimeFormat('en-US', { dateStyle: 'medium' })
 const datetimeFormat = new Intl.DateTimeFormat('en-US', { dateStyle: 'short' })
@@ -8,11 +10,20 @@ export async function getNotes(count?: number): Promise<Note[]> {
   const sortedNotes = notes
     .sort((a, b) => b.data.publishDate.getTime() - a.data.publishDate.getTime())
     .map((aNote) => {
+      // Not using remark so we can use the reading time in the note list.
+      let readingTimeMinutes = Math.floor(readingTime(aNote.body).minutes)
+
+      if (readingTimeMinutes < 1) {
+        readingTimeMinutes = 1
+      }
+
       const note: Note = {
         ...aNote,
         href: `/notes/${aNote.slug}`,
         publishDate: dateFormat.format(aNote.data.publishDate),
         publishDatetime: datetimeFormat.format(aNote.data.publishDate),
+        readingDatetime: serialize({ minutes: readingTimeMinutes }),
+        readingTime: `${readingTimeMinutes}min`,
       }
 
       if (aNote.data.updateDate) {
@@ -34,6 +45,8 @@ export type Note = CollectionEntry<'notes'> & {
   href: string
   publishDate: string
   publishDatetime: string
+  readingDatetime: string
+  readingTime: string
   updateDate?: string
   updateDatetime?: string
 }
