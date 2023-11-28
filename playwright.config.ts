954c1f05d57abd2388e228922e47cbc95277792f
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2023-11-28 14:52:47 +0100

    test: enable playwright headless mode

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'], headless: false },
+      use: { ...devices['Desktop Chrome'] },
     },
   ],
   use: {
