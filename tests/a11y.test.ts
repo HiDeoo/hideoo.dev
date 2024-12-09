1f565185172a0dff2b26552464fcb6f79de5c2e8
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-12-09 09:24:15 +0100

    test: update a11y tests

--- tests/a11y.test.ts
+++ tests/a11y.test.ts
@@ -13,8 +13,25 @@ for (const theme of ['dark', 'light'] as const) {
 
         const results = await new AxeBuilder({ page }).analyze()
 
-        expect(results.violations).toEqual([])
+        expect(filterViolations(results.violations)).toEqual([])
       })
     }
   })
 }
+
+function filterViolations(violations: Violations) {
+  return violations.filter((violation) => {
+    /**
+     * Ignore the `landmark-complementary-is-top-level` violation that we use for asides.
+     *
+     * The best action to fix this violation would be to remove the landmark altogether as it's not necessary in this
+     * case and switch to the `note` role. Although, this is not possible at the moment due to an issue with NVDA not
+     * announcing it and also skipping the associated label for a role not supported.
+     *
+     * @see https://github.com/nvaccess/nvda/issues/10439
+     */
+    return violation.id !== 'landmark-complementary-is-top-level'
+  })
+}
+
+type Violations = Awaited<ReturnType<typeof AxeBuilder.prototype.analyze>>['violations']
